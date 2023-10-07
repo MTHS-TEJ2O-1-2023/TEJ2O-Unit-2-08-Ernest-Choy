f8ea558094e05985920e923ce478a91c7179e4a7
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-06 22:27:43 -0400

    TYEPSCRIPTVER

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ernest
+ * Created on: Oct 2023
+ * This program sequences of all the colors in the RGB LED
 */
 
 basic.clearScreen()
@@ -11,33 +11,33 @@ basic.showIcon(IconNames.Happy)
 input.onButtonPressed(Button.A, function() {
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P16, 1)
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P12, 1)
   basic.showString("White")
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P16, 0)
-  pins.digitalWritePin(DigitalPin.P19, 0)
+  pins.digitalWritePin(DigitalPin.P12, 0)
   basic.showString("Red")
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P16, 1)
-  pins.digitalWritePin(DigitalPin.P19, 0)
+  pins.digitalWritePin(DigitalPin.P12, 0)
   basic.showString("Green")
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P16, 0)
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P12, 1)
   basic.showString("Blue")
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P16, 0)
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P12, 1)
   basic.showString("Magenta")
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P16, 1)
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P12, 1)
   basic.showString("Teal")
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P16, 1)
-  pins.digitalWritePin(DigitalPin.P19, 0)
+  pins.digitalWritePin(DigitalPin.P12, 0)
   basic.showString("Yellow")
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P16, 0)
-  pins.digitalWritePin(DigitalPin.P19, 0)
+  pins.digitalWritePin(DigitalPin.P12, 0)
 })
